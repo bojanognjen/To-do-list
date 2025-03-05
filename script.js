99ca6d967f71334a03def7db1d68aaac0bade726
Bojan Lukic <bojan.ognjen@gmail.com>
2025-03-05 21:39:28 +0100

    Removing li elements added

--- script.js
+++ script.js
@@ -8,13 +8,41 @@ function populateDOM(data) {
   data.forEach((item) => appendItem(item));
 }
 
+function remove(e) {
+  e.target.parentNode.remove();
+  let siblingText = e.target.parentNode.querySelector('label').innerText;
+  const found = dataArray.find(el => el.task == siblingText);
+
+  found ? (dataArray.splice(dataArray.indexOf(found), 1), console.log("Element removed from dataArray:", found)) : null;
+}
+
 let appendItem = (item) => {
   const todo = document.createElement('LI');
-   todo.innerHTML = `
-     <input type="checkbox" ${item.completed ? 'checked' : ''} />
-     <span>${item.task}</span>
-     <img class="cross" src="images/cross.png"></img>
-   `
+
+  // Create checkbox input
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  const uniqueId = "task-" + Date.now(); // Unique ID
+  checkbox.id = uniqueId;
+  checkbox.checked = `${item.completed ? 'checked' : ''}`;
+
+  // Create label
+  const label = document.createElement("label");
+  label.htmlFor = uniqueId; // Link label to input
+  label.textContent = `${item.task}`;
+
+  // Create image
+  const img = document.createElement("img");
+    img.className = "cross";
+    img.src = "images/close-outline.svg";
+
+  // Add click event to remove li when X is clicked
+  img.addEventListener("click", (e) => remove(e));  
+
+  todo.appendChild(checkbox);
+  todo.appendChild(label); 
+  todo.appendChild(img);
+
   container.appendChild(todo);
 }
 
@@ -32,5 +60,5 @@ function main() {
   addButton.addEventListener('click', pushArray);
 }
 
-window.addEventListener('load', main)
+window.addEventListener('load', main);
 
